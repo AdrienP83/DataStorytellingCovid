c5675223c3e643bee141378da01c4a5ea8f9bd94
Thomas Leydet <tomleydet@icloud.com>
2022-12-16 10:46:49 +0100

    test graph main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+siteHeader = st.beta_container()
+dataExploration = st.beta_container()
+newFeatures = st.beta_container()
+modelTraining = st.beta_container()
+
+st.text("Hello, welcome to my app!")
