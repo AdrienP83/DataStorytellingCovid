c00af09b29711b4ade97e2e617e2131e8293a221
Thomas Leydet <tomleydet@icloud.com>
2023-01-03 11:20:12 +0100

    add france.py + map europe

--- europe.py
+++ europe.py
@@ -1,4 +1,17 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
+df = pd.read_csv("owid/owid-covid-data.csv")
+df.columns
+
+## code
 st.markdown("# Page Europe 🇪🇺")
 st.sidebar.markdown("# Page Europe 🇪🇺")
+
+st.markdown("### Évolution des différents aspects du Covid-19 en Europe")
+df = pd.DataFrame(
+    np.random.randn(0, 2) / [500, 500] + [43.509576948063675, 5.421613454818726],
+    columns=['lat', 'lon'])
+
+st.map(df)
